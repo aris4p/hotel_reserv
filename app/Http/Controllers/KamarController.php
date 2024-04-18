ea8605f1ea0ab1cd8320e02e98b9a104675dec5a
aris4p <arisanggara72@gmail.com>
2024-04-19 05:40:53 +0700

    update 1.1

--- app/Http/Controllers/KamarController.php
+++ app/Http/Controllers/KamarController.php
@@ -20,9 +20,14 @@ class KamarController extends Controller
         $kamars = Kamar::query();
         return DataTables::of($kamars)
         ->addIndexColumn()
-        ->addColumn('action', function($kamar){
-            return '<button>Edit</button>';
-        })
+         ->addColumn('action', function ($row) {
+                // kita tambahkan button edit dan hapus
+                $btn = '<a href="'.route('edit-kamar', $row->id).'"  class="edit btn btn-primary btn-sm editProduk"><i class="fa fa-edit"></i>Edit</a>';
+
+                $btn .= ' <a href="javascript:void(0)" data-id="' . $row->id . '" class="btn btn-danger btn-sm deleteProduk"><i class="fa fa-trash"></i>Delete</a>';
+
+                return $btn;
+            })
         ->rawColumns(['action'])
         ->make(true);
     }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::prefix('admin')->group(function (){
 
     Route::get('/kamar', [KamarController::class, 'index'])->name('kamar');
     Route::get('/kamar/getdatatables', [KamarController::class, 'datatable'])->name('datatable');
+    Route::get('/kamar/edit/{id}', [KamarController::class, 'edit'])->name('edit-kamar');
 
     Route::post('/kamar', [KamarController::class, 'add'])->name('kamar_add');
 
